07c840c59b9505d96044b8559f9d41dffc829265
杨文鼎 <superywd@163.com>
2019-03-25 10:48:24 +0800

    初步添加

---
+++ lottery-manage/src/main/java/com/superywd/lottery/manage/LotteryManageApplication.java
@@ -0,0 +1,13 @@
+package com.superywd.lottery.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryManageApplication.class, args);
+    }
+
+}
