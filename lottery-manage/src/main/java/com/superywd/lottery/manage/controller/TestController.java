9a6bc17664f63636326f10b67a398aaa4b4364fe
杨文鼎 <superywd@163.com>
2019-03-26 21:49:58 +0800

    一通添加

---
+++ lottery-manage/src/main/java/com/superywd/lottery/manage/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.superywd.lottery.manage.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: 迷宫的中心
+ * @date: 2019/3/26 15:42
+ */
+@RequestMapping(path = "/test")
+@RestController
+public class TestController {
+
+
+    @RequestMapping(path = "/test1",method = RequestMethod.GET)
+    public Object testApi(){
+        return "测试一下";
+    }
+
+
+}
