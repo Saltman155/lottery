2bace997eee5aee9eb9d72171c3f097471c4a762
杨文鼎 <superywd@163.com>
2019-05-14 19:29:48 +0800

    更新前端

--- lottery-manage/src/main/resources/static/assets/js/pages/main.js
+++ lottery-manage/src/main/resources/static/assets/js/pages/main.js
@@ -1,24 +1,42 @@
 
-//vue框架
-var vm = new Vue({
+jQuery(function() {
+    $("#exit-system-btn").on("click",$.exitSystem);
+    $("#load-activity-btn").on("click",$.loadActivityList);
+    $("#load-user-btn").on("click",$.loadUserBtn);
+    // $("#activity-register-btn").on("click",)
+});
 
-    el:'#page-container',
-    methods:{
-        //退出登录
-        loginOut:function (event) {
-            console.log("退出登录");
-            $.ajax({
-                type:'GET',
-                url:'/lottery/admin/exit',
-                success:function () {
-                    window.location.href = "./login.html";
-                },
-                error:function () {
-                    console.log("退出异常...")
-                    window.location.href = "./login.html";
-                }
-            })
+
+
+$.exitSystem = function(){
+    console.log("退出登录");
+    $.ajax({
+        type:'GET',
+        url:'/lottery/admin/exit',
+        success:function () {
+            window.location.href = "./login.html";
+        },
+        error:function () {
+            console.log("退出异常...");
+            window.location.href = "./login.html";
         }
-    }
+    })
+}
 
-})
+$.loadActivityList = function(){
+    console.log("载入活动列表");
+    $("#main-container").load("activity_list.html");
+}
+
+$.loadUserBtn = function(){
+    console.log("载入用户列表");
+    $("#main-container").load("user_list.html");
+}
+
+
+$.activityRegister = function(){
+    console.log("载入活动注册");
+    // $("#main-container").load("index.html");
+}
+
+
